fc46a3efbc821225a4800d353509bef6c2398206
Vikas Bhavsar <vikasbhvsr@gmail.com>
2020-12-27 18:29:39 -0800

    minor changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   env: {
-    MONGO_URI:
-      'mongodb+srv://vikasbhvsr:G7Mtkk4b13kMK23E@vikascluster.ck21o.mongodb.net/rsvp-app?retryWrites=true&w=majority',
+    MONGO_URI: process.env.MONGO_URI,
   },
 };
