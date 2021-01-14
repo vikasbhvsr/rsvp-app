db757827011b458b4c975f25e7608f1d7affea7c
Vikas Bhavsar <vikasbhvsr@gmail.com>
2021-01-13 22:04:51 -0800

    async sendemail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,7 +2,7 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendEmail = (to, from, name) => {
+async function sendEmail(to, from, name) {
   const message = {
     to: to,
     from: from,
@@ -13,10 +13,27 @@ const sendEmail = (to, from, name) => {
     },
   };
 
-  sgMail
+  await sgMail
     .send(message)
     .then((response) => console.log(response))
     .catch((error) => console.log(error));
-};
+}
+
+// const sendEmail = (to, from, name) => {
+//   const message = {
+//     to: to,
+//     from: from,
+//     templateId: process.env.SENDGRID_TEMPLATE_ID,
+//     dynamic_template_data: {
+//       subject: `Vaidehi's Baby Shower - RSVP Confirmation`,
+//       name: name,
+//     },
+//   };
+
+//   sgMail
+//     .send(message)
+//     .then((response) => console.log(response))
+//     .catch((error) => console.log(error));
+// };
 
 module.exports = sendEmail;
