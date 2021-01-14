220594df4d916b362d0447918048e623df9c8500
Vikas Bhavsar <vikasbhvsr@gmail.com>
2021-01-13 22:11:29 -0800

    async sendsms

--- utils/sendSMS.js
+++ utils/sendSMS.js
@@ -1,15 +1,26 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
-const sendSMS = (phone, message) => {
-  const client = require('twilio')(accountSid, authToken);
-  client.messages
+async function sendSMS(phone, message) {
+  await client.messages
     .create({
       body: message,
       from: process.env.TWILIO_PHONE_NUMBER,
       to: phone,
     })
     .then((message) => console.log(message));
-};
+}
+
+// const sendSMS = (phone, message) => {
+//   const client = require('twilio')(accountSid, authToken);
+//   client.messages
+//     .create({
+//       body: message,
+//       from: process.env.TWILIO_PHONE_NUMBER,
+//       to: phone,
+//     })
+//     .then((message) => console.log(message));
+// };
 
 module.exports = sendSMS;
